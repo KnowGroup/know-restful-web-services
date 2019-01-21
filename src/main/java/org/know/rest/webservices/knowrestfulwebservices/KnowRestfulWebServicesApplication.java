e3177939c48c62bd1dc4914ac0ad5ee155d679ab
KnowGroup <45757509+KnowGroup@users.noreply.github.com>
2019-01-21 14:47:21 +0530

    Internationalization
    Implemented internationalization using
    ResourceBundleMessageSource , SessionLocaleResolver, MessageSource and @RequestHeader("Accept-Language") Locale

--- src/main/java/org/know/rest/webservices/knowrestfulwebservices/KnowRestfulWebServicesApplication.java
+++ src/main/java/org/know/rest/webservices/knowrestfulwebservices/KnowRestfulWebServicesApplication.java
@@ -1,7 +1,13 @@
 package org.know.rest.webservices.knowrestfulwebservices;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class KnowRestfulWebServicesApplication {
@@ -9,6 +15,20 @@ public class KnowRestfulWebServicesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KnowRestfulWebServicesApplication.class, args);
 	}
+        
+        @Bean
+        public LocaleResolver localeResolver(){
+            final SessionLocaleResolver localeResolver = new SessionLocaleResolver(); 
+            localeResolver.setDefaultLocale(Locale.US);
+            return localeResolver;
+        }
+        
+        @Bean
+        public ResourceBundleMessageSource messageSource(){
+            final ResourceBundleMessageSource rbms = new ResourceBundleMessageSource();
+                rbms.setBasename("messages");
+            return rbms;
+        }
 
 }
 
--- src/main/java/org/know/rest/webservices/knowrestfulwebservices/controllers/Lesson_00_HelloWorldController.java
+++ src/main/java/org/know/rest/webservices/knowrestfulwebservices/controllers/Lesson_00_HelloWorldController.java
@@ -1,7 +1,11 @@
 package org.know.rest.webservices.knowrestfulwebservices.controllers;
 
+import java.util.Locale;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -23,6 +27,14 @@ public class Lesson_00_HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorld helloWorldBean(){
         return new HelloWorld("Hello World ! Let us know all.");
+    }
+
+    @Autowired
+    private MessageSource messageSource;
+    
+    @GetMapping(path = "/hello-world-international")
+    public String helloWorldInternationlized(@RequestHeader(name = "Accept-Language",required = false) Locale locale){
+        return messageSource.getMessage("hello-world-message",null,locale);
     }
     
     /**
